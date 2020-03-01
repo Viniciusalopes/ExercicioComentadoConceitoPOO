bc6988eda6bb4acf09a0daf44cf6b95db7db2bbf
MiguellNeto <56120625+MiguellNeto@users.noreply.github.com>
2020-03-01 03:01:37 -0300

    ConceitoPOO
    Conceito aprendido em sala de aula até 01/03/2020 , usei como exemplo o exercicio da Esfera  comentado e estruturado .
    
    Desenvolva, usando a UML (diagrama de classe), uma classe que modele um objeto esfera em conformidade com o paradigma orientado a objeto. Posteriormente implemente esta classe. A classe deverá conter as seguintes características: raio e o cálculo da área e do volume da esfera. Obs.: o valor do raio não pode ser negativo. A área total = (4*3.1415*raio2) e volume =((4/3)*3.1415*raio3).

---
+++ ConceitoPOO/src/classes/Conceito.java
@@ -0,0 +1,88 @@
+/*
+ * Conceitos de POO visto em sala de aula .
+ */
+package classes;  //IDEIA
+
+/*
+
+Para construir uma classe é preciso utilizar o pilar da abstração.
+Uma Classe é uma descrição de um conjunto de objetos que compartilham os
+mesmos atributos, operações, métodos .
+
+*/
+
+
+public class Conceito { 
+    
+    // ATRIBUTOS ================> são as características de um objeto. Um atributo é uma variável que pertence 
+                                    // a um objeto. Os dados de um objeto são armazenados nos seus atributos.
+                                                             // ( SUBSTANTIVO )
+    
+   
+    
+    private float raio = 0 ;      // VISIBILIDADE ===> São distribuídas em três tipos, 
+                                      // private Todo Atributo deve ser privado uma função local e um único bloco de código
+                                      // public Todo Método deve ser publico visível para tudo uma função a ser chamada a qualquer momento
+                                      // protect (protegido) esse caso restringe o parâmetro fora da classe, mas ainda acessível as suas subclasses (herança)
+     
+    
+    
+                                  // ÒBJETO ===> são características definidas pelas classes. 
+                                  // neles é permitido instanciar objetos da classe para inicializar 
+                                  // os atributos e invocar os métodos. Objeto só e executado quando chamdo pela uma mensagem.
+                                            // ( INDIVIDUALIZOU OBJETO )
+                                            // ( TODO OBJETO É AUTÔNOMO )    
+    
+
+     // METODOS ==================> são as ações que os objetos podem exercer quando solicitados, 
+                                     // onde podem interagir e se comunicarem com outros objetos
+                                                        //  (VERBO E AÇÃO)
+    
+    
+    
+    /**
+     * @return the raio
+     */
+    public float getRaio() {               // ENCAPSULAMENTO ===> Restringir o acesso do comportameto interno de um Objeto.
+        return raio;                       // Sempre com os métodos privados ligadas a métodos especiais chamados getters e setters,
+                                           // que irão retornar e setar o valor da propriedade, respectivamente. O encapsulamento evita 
+                                           // o acesso direto a propriedade do objeto, adicionando uma outra camada de segurança à aplicação.
+    }
+                                            
+                                            
+    /**                             
+     * @param raio the raio to set             
+     */                   
+   
+    //trow grita   
+    //Throws é o método lançador de execões , pode  ter vários throws dentro do método     
+    //set é onde faz a atribuição do valor do raio, por isso entra no set o tratamento de exeções  
+   
+    public void setRaio(float raio) throws Exception{  //Todo throw esta ligado ao objeto Exception
+        if (raio < 0 )throw new Exception("Raio não pode ser negativo");
+       //parametro raio 
+        this.raio = raio; // this só pode colocar ao lado de atributo (Escopo maior da classe) ou seja, enxerga outro escopo.
+                          // Tudo entre { Abre e fecha chave } é um escopo 
+    }
+   
+    
+    public float calcularArea(){
+        
+       return  (float) (4*3.1415*(raio*raio));  // conjunto inteiro + conjunto double sempre vai ser double. Usa (float) para converter. 
+                                                
+        
+    }
+    
+    public float calcularVolume(){
+        
+       return (float) ((4.0/3.0)*3.1415*(raio*raio*raio)); //ou Math.pow(raio, 3)
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
